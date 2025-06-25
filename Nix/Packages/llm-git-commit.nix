db8630eb0a8ffbad3946f1bd72c760a69b4fc2ba
Immelancholy <lenalowes0@gmail.com>
2025-06-25 22:03:54 +0100

    Just try this

--- Nix/Packages/llm-git-commit.nix
+++ Nix/Packages/llm-git-commit.nix
@@ -3,40 +3,35 @@
   pkgs,
   lib,
   ...
-}: let
-  fs = lib.fileset;
-  sourceFiles = ../../.;
-in
-  fs.trace sourceFiles
-  python3Packages.buildPythonApplication {
-    name = "llm-git-commit";
-    version = "0.1.4";
-    pyproject = true;
-    src = fs.toSource {
-      root = ../../.;
-      fileset = sourceFiles;
-    };
+}:
+python3Packages.buildPythonApplication rec {
+  name = "llm-git-commit";
+  version = "0.1.4";
+  pyproject = true;
 
-    nativeBuildInputs = with pkgs.python3Packages; [
-      setuptools
-      wheel
-    ];
+  src = ../../dist/llm_git_commit-0.1.1.tar.gz;
 
-    propagatedBuildInputs = with pkgs.python3Packages; [
-      click
-      llm
-      prompt-toolkit
-    ];
+  doCheck = false;
 
-    dontCheckRuntimeDeps = true;
+  build-system = with pkgs.python3Packages; [
+    setuptools
+  ];
 
-    meta = with lib; {
-      description = ''
-        a plugin for SimonW llm CLI which analyses diffs in a local git repository , generates commit messages in an interactive prompt & commits
-      '';
-      homepage = "https://github.com/ShamanicArts/llm-git-commit.git";
-      license = licenses.mit;
-      maintainers = with maintainers; [Immelancholy];
-      mainProgram = "llm-git-commit";
-    };
-  }
+  dependencies = with pkgs.python3Packages; [
+    click
+    llm
+    prompt-toolkit
+  ];
+
+  dontCheckRuntimeDeps = true;
+
+  meta = with lib; {
+    description = ''
+      a plugin for SimonW llm CLI which analyses diffs in a local git repository , generates commit messages in an interactive prompt & commits
+    '';
+    homepage = "https://github.com/ShamanicArts/llm-git-commit.git";
+    license = licenses.mit;
+    maintainers = with maintainers; [Immelancholy];
+    mainProgram = "llm-git-commit";
+  };
+}
